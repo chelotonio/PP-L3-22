998da988c5b10093d235165b4f9bb9ecb551ba76
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-05-17 12:06:19 -0400

    Creación de clases principales

---
+++ DobbleProject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("POR FIN  JAVA OEEE");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Ahora i es: " + i);
+        }
+    }
+}
